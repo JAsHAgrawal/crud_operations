f69accb52270d0246354251e7258eb849d29a13a
Jash Agrawal <jash.agarwal@aiab.in>
2021-11-14 19:05:22 +0530

    env removed

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,15 +3,12 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     // mongodb connection string
-    const con = await mongoose.connect(
-      "mongodb+srv://xpainx:1234@cluster0.3bcpj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-      }
-    );
+    const con = await mongoose.connect("your mongo url here", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
 
     console.log(`MongoDB connected : ${con.connection.host}`);
   } catch (err) {
